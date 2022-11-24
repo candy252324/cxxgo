fbace2a23f96dbb1fb1927a492e2e7acbc61b241
cjh <1964646121@qq.com>
2022-11-24 23:13:34 +0800

    获取版本更新

--- packages/core/cli/lib/index.js
+++ packages/core/cli/lib/index.js
@@ -1,6 +1,8 @@
 'use strict'
-
-// const utils = require('@cxxgo/utils')
+const axios = require('axios')
+const utils = require('@cxxgo/utils')
+const log = utils.log
+const fs = require('fs')
 
 module.exports = core
 
@@ -9,4 +11,19 @@ function core(a) {
 }
 
 /** 检查版本号更新 */
-function checkPkgVersion() {}
+function checkPkgVersion() {
+  const pkgVersion = require('../../package.json').version
+  axios.get('https://registry.npmjs.org/vue').then(res => {
+    if (res.status === 200) {
+      debugger
+      console.log(typeof JSON.parse(res.data))
+      console.log(res.data)
+    }
+
+    // if (res.status === 200) {
+    //   const versions = Object.keys(res.data.versions || [])
+    //   console.log(versions)
+    // }
+  })
+  log.info('检查版本更新', pkgVersion)
+}
